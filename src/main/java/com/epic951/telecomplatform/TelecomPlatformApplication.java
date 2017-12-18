b805b00af3bbfe11bd871eebd5132fc87cdb3585
epic951 <inertialism@gmail.com>
2017-12-18 17:19:09 +0200

    Initial stage after using Spring Initializer to select the most helpful modules to accomplish the various tasks

---
+++ src/main/java/com/epic951/telecomplatform/TelecomPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.epic951.telecomplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomPlatformApplication.class, args);
+	}
+}
---
+++ src/test/java/com/epic951/telecomplatform/TelecomPlatformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epic951.telecomplatform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TelecomPlatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
