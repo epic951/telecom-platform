68cb25f89fe2422cc9a064e09d5fbf240c53b6c7
epic951 <inertialism@gmail.com>
2017-12-21 08:46:54 +0200

    fixed dependency issue with maven

--- src/main/java/com/epic951/utilities/HTTPUtilities.java
+++ src/main/java/com/epic951/utilities/HTTPUtilities.java
@@ -1,21 +1,21 @@
 package com.epic951.utilities;
 
-import java.io.IOException;
+//import java.io.IOException;
 import java.nio.charset.Charset;
 
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
+//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+//import org.springframework.mock.http.MockHttpOutputMessage;
 
 public class HTTPUtilities {
 
 	public static final MediaType JSON_CONTENT_TYPE = new MediaType(MediaType.APPLICATION_JSON.getType(),
 			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
-	public static String json(Object o) throws IOException {
-		MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-		MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
-		jsonConverter.write(o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
-		return mockHttpOutputMessage.getBodyAsString();
-	}
+//	public static String json(Object o) throws IOException {
+//		MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+//		MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+//		jsonConverter.write(o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+//		return mockHttpOutputMessage.getBodyAsString();
+//	}
 }
