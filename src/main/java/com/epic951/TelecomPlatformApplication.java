4001bfb1fe192d86afcea1b8c90245502454cb6d
epic951 <inertialism@gmail.com>
2017-12-25 11:22:50 +0200

    Added global cors support as a bean

--- src/main/java/com/epic951/TelecomPlatformApplication.java
+++ src/main/java/com/epic951/TelecomPlatformApplication.java
@@ -2,11 +2,27 @@ package com.epic951;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class TelecomPlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TelecomPlatformApplication.class, args);
+	}
+
+	// Enabling Global CORS support
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:4200").allowCredentials(true)
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
 	}
 }
