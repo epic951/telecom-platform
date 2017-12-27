804da42784bc17996145c5e43e30ef3efcc0889d
epic951 <inertialism@gmail.com>
2017-12-27 10:09:26 +0200

    Altred mapping

--- src/main/java/com/epic951/business/controllers/RedirectController.java
+++ src/main/java/com/epic951/business/controllers/RedirectController.java
@@ -2,8 +2,10 @@ package com.epic951.business.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/")
 public class RedirectController {
 
 	@GetMapping(value = "/")
