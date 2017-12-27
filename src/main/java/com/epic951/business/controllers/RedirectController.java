a0ce2796d4a900b33e50c883f49a4dc99057101e
epic951 <inertialism@gmail.com>
2017-12-27 10:06:21 +0200

    Now Forwarding to the Angular Front-end

---
+++ src/main/java/com/epic951/business/controllers/RedirectController.java
@@ -0,0 +1,13 @@
+package com.epic951.business.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RedirectController {
+
+	@GetMapping(value = "/")
+	public String forwardToAngularFrontEnd() {
+		return "forward:/https://telecom-platform-frontend.herokuapp.com/";
+	}
+}
