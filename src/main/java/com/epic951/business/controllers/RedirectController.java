4e14a578f36b665a4686ef7a08f5942db14920a3
epic951 <inertialism@gmail.com>
2017-12-27 10:51:48 +0200

    Altred mapping

--- src/main/java/com/epic951/business/controllers/RedirectController.java
+++ src/main/java/com/epic951/business/controllers/RedirectController.java
@@ -4,14 +4,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RedirectController {
 
 	@GetMapping(value = "/")
-	public HttpServletResponse forwardToAngularFrontEnd(HttpServletResponse response) {
-		response.setHeader("Location", "https://telecom-platform-frontend.herokuapp.com/");
-		return response;
-		// return "forward:https://telecom-platform-frontend.herokuapp.com/";
+	public ModelAndView forwardToAngularFrontEnd() {
+		return new ModelAndView("forward:https://telecom-platform-frontend.herokuapp.com");
+		// return "redirect:https://telecom-platform-frontend.herokuapp.com/";
 	}
 }
